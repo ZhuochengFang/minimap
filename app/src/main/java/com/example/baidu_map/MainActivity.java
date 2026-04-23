7741cd7b30d9f519eac38bc666ea7e65a29b419e
ZhuochengFang <3220105794@zju.edu.cn>
2026-04-23 11:56:33 +0800

    map type switchable

--- app/src/main/java/com/example/baidu_map/MainActivity.java
+++ app/src/main/java/com/example/baidu_map/MainActivity.java
@@ -1,35 +1,78 @@
 package com.example.baidu_map;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
+import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.MapView;
 
 public class MainActivity extends AppCompatActivity {
     private MapView mMapView = null;
+    private RadioGroup mapType;
+    private RadioButton normalBtn;
+    private RadioButton satelliteBtn;
+    private CheckBox trafficEnabled;
+    private CheckBox heatMapEnabled;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mMapView=(MapView) findViewById(R.id.mapView);
+        mMapView = findViewById(R.id.bmapView);
+        mapType = findViewById(R.id.id_rp_maptype);
+        normalBtn = findViewById(R.id.id_btn_normal);
+        satelliteBtn = findViewById(R.id.id_btn_satellite);
+        trafficEnabled = findViewById(R.id.id_cb_trafficEnabled);
+        heatMapEnabled = findViewById(R.id.id_cb_heatMapEnabled);
+        initEvent();
     }
+
+    private void initEvent() {
+        mapType.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (i == normalBtn.getId()) {
+                    mMapView.getMap().setMapType(BaiduMap.MAP_TYPE_NORMAL);
+                } else if (i == satelliteBtn.getId()) {
+                    mMapView.getMap().setMapType(BaiduMap.MAP_TYPE_SATELLITE);
+                }
+            }
+        });
+
+        trafficEnabled.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                mMapView.getMap().setTrafficEnabled(b);
+            }
+        });
+
+        heatMapEnabled.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                mMapView.getMap().setBaiduHeatMapEnabled(b);
+            }
+        });
+    }
+
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
         mMapView.onResume();
     }
+
     @Override
-    protected void onPause(){
+    protected void onPause() {
         super.onPause();
         mMapView.onPause();
     }
+
     @Override
-    protected void onDestroy(){
+    protected void onDestroy() {
         super.onDestroy();
         mMapView.onDestroy();
     }
